a4f9dc60a2a2f2a4fa1a2e4df21a7f5d4561b521
Subhajit Sahu <wolfram77@gmail.com>
2025-12-23 10:33:52 +0530

    update version

--- build.sh
+++ build.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
+# Fetch the latest version of the library
+fetch() {
 URL="https://raw.githubusercontent.com/tanakh/cmdline/refs/heads/master/cmdline.h"
+if [ -f "cmdline.h" ]; then return; fi
 
-# Build command
-build() {
 # Download the release
-if [ ! -f "cmdline.h" ]; then
-  echo "Downloading cmdline.h from $URL ..."
-  curl -L "$URL" -o "cmdline.h"
-  echo ""
-fi
+echo "Downloading cmdline.h from $URL ..."
+curl -L "$URL" -o "cmdline.h"
+echo ""
 }
 
 
-# Test command
+# Test the project
 test() {
-  echo "Testing cmdline.cxx ..."
-  clang++ -std=c++17 -I. examples/01-test.cxx -o test
-  ./test
-  clang++ -std=c++17 -I. examples/02-test.cxx -o test
-  ./test
-  echo ""
+echo "Running 01-basic.cxx ..."
+clang++ -std=c++17 -I. -o 01.exe examples/01-basic.cxx    && ./01 && echo -e "\n"
+echo "Running 02-advanced.cxx ..."
+clang++ -std=c++17 -I. -o 02.exe examples/02-advanced.cxx && ./02 && echo -e "\n"
 }
 
 
-# Main
-if [[ "$1" == "test" ]]; then test; else build; fi
+# Main script
+if [[ "$1" == "test" ]]; then test
+elif [[ "$1" == "fetch" ]]; then fetch
+else echo "Usage: $0 {fetch|test}"; fi
